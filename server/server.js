ccee248a553f7de85a2418d40b0e28783726e4aa
elgnis3900 <jonathanduarte313@gmail.com>
2021-02-06 22:35:00 -0500

    setting up express and postgresql

--- server/server.js
+++ server/server.js
@@ -1,10 +1,72 @@
 const express = require("express");
-
+const morgan = require('morgan');
+require("dotenv").config();
 const app = express();
 
-const port = 3005;
+// Middleware
 
+app.use(express.json());
+
+// Get All Pizzas
+
+app.get("/api/v1/pizzas", (req, res) => {
+  // console.log("Route Handler to Get All Pizzas is now running")
+  res.status(200).json({
+    status: "success",
+    data: {
+      pizza: ["cheese pizza, pepperoni"],
+    },
+  });
+});
+
+// Get a Pizza
+
+app.get("/api/v1/pizzas/:id", (req, res) => {
+  console.log(req.params);
+
+  res.status(200).json({
+    data: {
+      pizza: "cheese",
+    },
+  });
+});
+
+// Create a Pizza
+
+app.post("/api/v1/pizzas", (req, res) => {
+  console.log(req.body);
+
+  res.status(201).json({
+    data: {
+      pizza: "cheese",
+    },
+  });
+});
+
+// Update Pizzas
+
+app.put("/api/v1/pizzas/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+
+  res.status(200).json({
+    data: {
+      pizza: "cheese",
+    },
+  });
+})
+
+// Delete Pizzas
+
+app.delete("/api/v1/pizzas/:id", (req, res) => {
+  res.status(204).json({
+    status: "success",
+  });
+});
+
+const hostname = 'localhost';
+const port = process.env.PORT || 3031;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+  console.log(`Server listening on host: ${hostname} port: ${port}`);
 });
 
