29a17ff0e7b5713d4b84f553a1eb3d0798a6d4d0
elgnis3900 <jonathanduarte313@gmail.com>
2021-02-19 18:25:52 -0500

    continued applying tailwind styles

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className="bg-red-200">
+    <div className="bg-white">
       <h1>Hello World</h1>
       <h2>Hello Universe</h2>
+      <h3>Hello Cosmos</h3>
     </div>
   )
 }
