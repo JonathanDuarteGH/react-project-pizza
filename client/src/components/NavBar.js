2bd8782597f39087f9131955e101766087099a64
elgnis3900 <jonathanduarte313@gmail.com>
2021-02-22 14:02:47 -0500

    continued applying tailwind styles 4

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className="bg-white flex justify-between">
-      <h1>Hello World</h1>
-      <h2>Hello Universe</h2>
-      <h3>Hello Cosmos</h3>
-    </div>
+    <nav className="border border-double border-gray-400 rounded bg-white flex flex-wrap px-4 py-5 w-full fixed z-10">
+      <h1>Menu</h1>
+      <h2>Special</h2>
+      <h3>Pie Rewards</h3>
+    </nav>
   )
 }
 
-export default NavBar
+export default NavBar;
