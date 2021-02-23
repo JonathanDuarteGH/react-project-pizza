b409c158ea7a742e8c00d5b5d76ada4a61ffc836
elgnis3900 <jonathanduarte313@gmail.com>
2021-02-23 10:51:08 -0500

    CSS

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <nav className="border border-double border-gray-400 rounded bg-white flex flex-wrap px-4 py-5 w-full fixed z-10">
+    <nav className="border border-double border-gray-400 rounded bg-white flex flex-wrap px-4 py-5 w-full fixed z-9">
       <h1>Menu</h1>
       <h2>Special</h2>
       <h3>Pie Rewards</h3>
