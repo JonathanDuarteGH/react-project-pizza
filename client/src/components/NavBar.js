ea3bcf3fb4aa807ea1fdeeaa6c148c5871b3467b
elgnis3900 <jonathanduarte313@gmail.com>
2021-05-11 06:57:23 -0400

    deleted special2 as placeholder

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,7 +5,6 @@ const NavBar = () => {
     <nav className="border border-double border-gray-400 rounded bg-white flex flex-wrap px-5 py-5 w-full fixed z-10">
       <h1>Menu</h1>
       <h2>Special</h2>
-      <h2>Special2</h2>
       <h3>Pie Rewards and Coupons</h3>
     </nav>
   )
